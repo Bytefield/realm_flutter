3ef8ac653b6382c42d8f4d4a120e8df8c4a6dd28
Bytefield <ramosperez1978@gmail.com>
2024-02-14 19:27:43 +0100

    added delete method to bloc

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:realm/realm.dart';
 
 import '../settings/settings_view.dart';
@@ -7,10 +8,12 @@ import 'sample_item_details_view.dart';
 
 class ListBloc {
   final RealmResults<SampleItem> items;
+  final Realm _realm;
+  ListBloc(this.items) : _realm = items.realm;
 
-  ListBloc(this.items);
-
-  void addNewItem() {}
+  void addNewItem() {
+    _realm.write(() => _realm.add(SampleItem(1 + (items.lastOrNull?.id ?? 1))));
+  }
 }
 
 class SampleItemListView extends StatelessWidget {
@@ -40,35 +43,70 @@ class SampleItemListView extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'sampleItemListView',
-        itemCount: bloc.items.length,
-        itemBuilder: (context, int index) {
-          final item = bloc.items[index];
+      body: StreamBuilder(
+          stream: bloc.items.changes,
+          builder: (context, snapshot) {
+            return ListView.builder(
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'sampleItemListView',
+              itemCount: bloc.items.length,
+              itemBuilder: (context, int index) {
+                final item = bloc.items[index];
 
-          return ListTile(
-              title: Text('SampleItem ${item.id}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-              ),
-              onTap: () {
-                // Navigate to the details page. If the user leaves and returns to
-                // the app after it has been killed while running in the
-                // background, the navigation stack is restored.
-                Navigator.restorablePushNamed(
-                  context,
-                  SampleItemDetailsView.routeName,
-                );
-              });
-        },
-      ),
+                return SampleItemTile(bloc: ItemBloc(item));
+              },
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: bloc.addNewItem,
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ItemBloc {
+  final SampleItem item;
+  final Realm _realm;
+  ItemBloc(this.item) : _realm = item.realm;
+
+  void deleteItem() {
+    _realm.write(() => _realm.delete(item));
+  }
+}
+
+class SampleItemTile extends StatelessWidget {
+  const SampleItemTile({
+    super.key,
+    required this.bloc,
+  });
+
+  final ItemBloc bloc;
+
+  @override
+  Widget build(context) {
+    final item = bloc.item;
+    return Dismissible(
+      key: ValueKey(item.id),
+      background: Container(color: Colors.red),
+      onDismissed: (direction) => bloc.deleteItem(),
+      child: ListTile(
+        title: Text('SampleItem ${item.id}'),
+        leading: const CircleAvatar(
+          // Display the Flutter Logo image asset.
+          foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+        ),
+        onTap: () {
+          // Navigate to the details page. If the user leaves and returns to
+          // the app after it has been killed while running in the
+          // background, the navigation stack is restored.
+          Navigator.restorablePushNamed(
+            context,
+            SampleItemDetailsView.routeName,
+          );
+        },
       ),
     );
   }
